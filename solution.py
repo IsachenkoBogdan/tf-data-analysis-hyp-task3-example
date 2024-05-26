9c9be5c321afcdb59f2e4b13f54e090015d9b790
IsachenkoBogdan <61412323+IsachenkoBogdan@users.noreply.github.com>
2024-05-26 20:09:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,11 +5,4 @@ from scipy.stats import ttest_1samp
 chat_id = 683820405 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    p_value = ttest_1samp(x, 300)[1]
-    alpha = 0.08
-    p_value /= 2
-    
-    if np.mean(x) >= 300:
-        return False
-    
-    return p_value < alpha
+    return ttest_1samp(x, 300, alternative='less').pvalue < 0.08 
