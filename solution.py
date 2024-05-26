6623d2d24c3f5a1c88299896fed01c5c1aba2a08
IsachenkoBogdan <61412323+IsachenkoBogdan@users.noreply.github.com>
2024-05-26 23:04:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ from scipy.stats import ttest_1samp
 chat_id = 683820405 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    return ttest_1samp(x, 300, alternative='greater').pvalue < 0.08 
+    return ttest_1samp(x, 300, alternative='less').pvalue < 0.08 
